2c5c848158ef6a36bc8a86701537a5d83af091f7
Piotr Ludynia <pietrekpl18@gmail.com>
2020-09-09 15:41:47 +0200

    create method for generate creditCardnumber and pin code

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,41 @@
 public class Account {
+    private long creditCardNumber;
+    private int pinCode;
+
+    public long getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(long creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+    public long generateCardNumber(){
+        long prefix = 400000L;
+        long suffix = (long) (Math.random() * 10000000000L);
+        String value = Long.valueOf(prefix)+""+Long.valueOf(suffix);
+        long cardNumber = Long.parseLong(value);
+        return cardNumber;
+    }
+    public int generatePIN(){
+        int randomPin = (int)(Math.random()*9000)+1000;
+        return randomPin;
+    }
+
+    public Account() {
+        System.out.println("Your card has been created");
+        System.out.println("Your card number:");
+        this.creditCardNumber = generateCardNumber();
+        System.out.println(this.creditCardNumber);
+        System.out.println("Your card PIN:");
+        this.pinCode = generatePIN();
+        System.out.println(this.pinCode);
+    }
 }
---
+++ src/main/java/MainMenu.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MainMenu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int menuOption = scanner.nextInt();
+
+        switch (menuOption){
+            case 1:
+                Account account = new Account();
+            case 2:
+
+            case 0:
+                System.out.println("Bye");
+                break;
+        }
+
+
+
+    }
+}
