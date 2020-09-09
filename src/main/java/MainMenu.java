8f8e3ee3f6d1a00cf4fc5d9da42369df2ee4c4ed
Piotr Ludynia <pietrekpl18@gmail.com>
2020-09-09 16:25:41 +0200

    update switch menu to work in loop

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class MainMenu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int menuOption = scanner.nextInt();
+        int menuOption ;
 
         Account account = new Account();
-        switch (menuOption){
-            case 1:
-                account.showInfo();
-                break;
-            case 2:
-                account.logIN();
-            case 0:
-                System.out.println("Bye");
-                break;
-        }
+        do {
+            menuOption = scanner.nextInt();
+            switch (menuOption){
+                case 1:
+                    account.showInfo();
+                    break;
+                case 2:
+                    account.logIN();
+                    break;
+                case 0:
+                    System.out.println("Bye");
+                    break;
+            }
+        }while (menuOption != 0);
+
 
 
 
