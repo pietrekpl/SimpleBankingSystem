3320f5995b95120ba1cd73cff1572d77303c2d98
Piotr Ludynia <pietrekpl18@gmail.com>
2020-09-12 17:41:41 +0200

    refactor, add class Account, Owner, Transaction

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,94 +1,10 @@
-import java.util.Random;
-import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
+import java.math.BigDecimal;
 
 public class Account {
-    private final Random random = new Random(System.currentTimeMillis());
-    private String creditCardNumber;
-    private int pinCode;
+    private long accountNumber;
+    private Owner owner;
+    private BigDecimal moneyAvailible;
+    private boolean isActive;
+    private Transaction transaction;
 
-
-    public String getCreditCardNumber() {
-        return creditCardNumber;
-    }
-
-    public void setCreditCardNumber(String creditCardNumber) {
-        this.creditCardNumber = creditCardNumber;
-    }
-
-    public int getPinCode() {
-        return pinCode;
-    }
-
-    public void setPinCode(int pinCode) {
-        this.pinCode = pinCode;
-    }
-    /* Assume our credit card's prefix should be 400000 and credit card's length as usually 16 */
-    public String generate(String prefix, int length) {
-        Random random = new Random(System.currentTimeMillis());
-        int randomNumberLength = length - (prefix.length() + 1);
-        StringBuilder builder = new StringBuilder(prefix);
-        for (int i = 0; i < randomNumberLength; i++) {
-            int digit = this.random.nextInt(10);
-            builder.append(digit);
-        }
-        int checkDigit = this.getCheckDigit(builder.toString());
-        builder.append(checkDigit);
-        return builder.toString();
-    }
-    // Luhn algorithm to check credit card validation
-    private int getCheckDigit(String number) {
-        int sum = 0;
-        for (int i = 0; i < number.length(); i++) {
-            int digit = Integer.parseInt(number.substring(i, (i + 1)));
-
-            if ((i % 2) == 0) {
-                digit = digit * 2;
-                if (digit > 9) {
-                    digit = (digit / 10) + (digit % 10);
-                }
-            }
-            sum += digit;
-        }
-        int mod = sum % 10;
-        return ((mod == 0) ? 0 : 10 - mod);
-    }
-    public String cardValidate(){
-
-        if (getCheckDigit(this.creditCardNumber) == 0){
-            return "Credit card number is valid";
-        }
-        return "Credit card number is Invalid";
-
-    }
-
-    public int generatePIN() {
-        int randomPin = (int) (Math.random() * 9000) + 1000;
-        return randomPin;
-    }
-
-    public void showInfo() {
-        System.out.println("Your card has been created");
-        System.out.println("Your card number:");
-        System.out.println(this.creditCardNumber);
-        System.out.println("Your card PIN:");
-        System.out.println(this.pinCode);
-    }
-
-    public void logIN() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your card number");
-        String enteredCardNumb = scanner.nextLine();
-        System.out.println("Enter your PIN:");
-        int enteredPIN = scanner.nextInt();
-        if (!enteredCardNumb.equals(this.creditCardNumber) || enteredPIN != this.pinCode) {
-            System.out.println("Wrong card number or PIN!");
-        }
-    }
-
-
-    public Account() {
-        this.creditCardNumber = generate("400000",16);
-        this.pinCode = generatePIN();
-    }
 }
---
+++ src/main/java/Owner.java
@@ -0,0 +1,44 @@
+public class Owner {
+    private String firstName;
+    private String lastName;
+    private String Address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public Owner(String firstName, String lastName, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        Address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", Address='" + Address + '\'' +
+                '}';
+    }
+}
