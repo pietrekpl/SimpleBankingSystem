44718b0caf8479b37e2b9afe0c26a951d48a11b7
Piotr Ludynia <pietrekpl18@gmail.com>
2020-09-13 18:58:17 +0200

    add properties to account, transaction

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -5,6 +5,54 @@ public class Account {
     private Owner owner;
     private BigDecimal moneyAvailible;
     private boolean isActive;
-    private Transaction transaction;
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public BigDecimal getMoneyAvailible() {
+        return moneyAvailible;
+    }
+
+    public void setMoneyAvailible(BigDecimal moneyAvailible) {
+        this.moneyAvailible = moneyAvailible;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber=" + accountNumber +
+                ", owner=" + owner +
+                ", moneyAvailible=" + moneyAvailible +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+    public Account(long accountNumber, Owner owner, BigDecimal moneyAvailible, boolean isActive) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.moneyAvailible = moneyAvailible;
+        this.isActive = isActive;
+    }
 
 }
--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,2 +1,47 @@
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Transaction {
+    private long accountNumber;
+    private BigDecimal value;
+    private LocalDateTime transactionDateAndTime;
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getTransactionDateAndTime() {
+        return transactionDateAndTime;
+    }
+
+    public void setTransactionDateAndTime(LocalDateTime transactionDateAndTime) {
+        this.transactionDateAndTime = transactionDateAndTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "accountNumber=" + accountNumber +
+                ", value=" + value +
+                ", transactionDateAndTime=" + transactionDateAndTime +
+                '}';
+    }
+
+    public Transaction(long accountNumber, BigDecimal value, LocalDateTime transactionDateAndTime) {
+        this.accountNumber = accountNumber;
+        this.value = value;
+        this.transactionDateAndTime = transactionDateAndTime;
+    }
 }
